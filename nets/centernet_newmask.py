9b861bc5cba3bb6bb2138515946419f50bd80515
itsmelingjun <125961434+itsmelingjun@users.noreply.github.com>
2025-11-24 23:43:41 +0800

    Update centernet_newmask.py

--- nets/centernet_newmask.py
+++ nets/centernet_newmask.py
@@ -15,13 +15,7 @@ class CenterNet_Resnet50(nn.Module):
         self.backbone = resnet50FPN(pretrained=pretrained)
         # 16,16,2048 -> 128,128,64
         self.decoder = resnet50_SEDecoder(inplanes=256)
-        # -----------------------------------------------------------------#
-        #   对获取到的特征进行上采样，进行分类预测、回归预测和mask预测
-        #   128, 128, 64 -> 128, 128, 64 -> 128, 128, num_classes
-        #                -> 128, 128, 64 -> 128, 128, 2
-        #                -> 128, 128, 64 -> 128, 128, 2
-        #                -> 128, 128, 64 -> 128, 128, num_classes
-        # -----------------------------------------------------------------#
+
         self.head = resnet50_maskHead(channel=64, num_classes=num_classes)
 
         self._init_weights()
@@ -160,4 +154,5 @@ class CenterNet_HourglassNet(nn.Module):
                 out[head] = self.__getattr__(head)[ind](cnv)
             outs.append(out)
         return outs
+
 
